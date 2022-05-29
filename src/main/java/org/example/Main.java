e17caeabc809fa512b2439e2332cd267f39ce8dc
student <student@localhost>
2022-05-29 19:07:19 +0300

    server template

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,22 @@
 package org.example;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Main {
   public static void main(String[] args) {
-    System.out.println("hello world");
+    // ServerSocket
+    // Socket
+    try (
+        final ServerSocket serverSocket = new ServerSocket(9999);
+    ) {
+      while (true) {
+        // блокирующий вызов
+        final Socket socket = serverSocket.accept();
+        System.out.println("accepted");
+      }
+    } catch (IOException e) {
+    }
   }
 }
